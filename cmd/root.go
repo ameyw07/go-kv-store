f88b35dfe0343a2c14d0f531c15f9c6edd15946a
ameyw07 <wadekaramey1997@gmail.com>
2026-01-21 04:56:35 +0900

    add devcontainer

---
+++ cmd/root.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"github.com/ameyw07/go-kv-store/pkg/server"
+)
+
+func Execute() {
+	server.Serve()
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/ameyw07/go-kv-store/cmd"
 
+func main() {
+	cmd.Execute()
 }
